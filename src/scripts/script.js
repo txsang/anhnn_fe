9292d90d8e73249d1ca9b517aea47d853d55b006
anhnn2401 <coolboy95.it@gmail.com>
2020-04-24 11:25:35 +0700

    fix/video gộp develop

--- src/scripts/script.js
+++ src/scripts/script.js
@@ -6,30 +6,18 @@ $('.slider-content').slick({
   slidesToShow: 1,
   slidesToScroll: 1
 });
-/*$(document).ready(function() {
-  $('.popup-youtube, .popup-vimeo, .popup-gmaps').magnificPopup({
-    disableOn: 700,
-    type: 'iframe',
-    mainClass: 'mfp-fade',
-    removalDelay: 160,
-    preloader: false,
-
-    fixedContentPos: false
-  });
-});*/
-
-  $('.video').magnificPopup({
-  type: 'iframe',
-  iframe: {
-     markup: '<div class="mfp-iframe-scaler">'+
-                '<div class="mfp-close"></div>'+
-                '<iframe class="mfp-iframe" frameborder="0" allowfullscreen></iframe>'+
-                '<div class="mfp-title">Some caption</div>'+
-              '</div>'
-  },
-  callbacks: {
-    markupParse: function(template, values, item) {
-     values.title = item.el.attr('title');
-    }
+$('.video').magnificPopup({
+type: 'iframe',
+iframe: {
+  markup: '<div class="mfp-iframe-scaler">'+
+            '<div class="mfp-close"></div>'+
+            '<iframe class="mfp-iframe" frameborder="0" allowfullscreen></iframe>'+
+            '<div class="mfp-title">Some caption</div>'+
+          '</div>'
+},
+callbacks: {
+  markupParse: function(template, values, item) {
+   values.title = item.el.attr('title');
   }
+}
 });
