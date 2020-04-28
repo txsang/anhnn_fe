9704e5ba6cd144a6a9c5c37fe2510a0728271862
anhnn2401 <coolboy95.it@gmail.com>
2020-04-28 10:03:15 +0700

    fix/slide fix/ button video fix/ testimonial

--- src/scripts/script.js
+++ src/scripts/script.js
@@ -6,6 +6,14 @@ $('.slider-content').slick({
   slidesToShow: 1,
   slidesToScroll: 1
 });
+$('.con-test').slick({
+  dots: true,
+  arrows: false,
+  infinite: true,
+  speed: 50,
+  slidesToShow: 1,
+  slidesToScroll: 1
+});
 $('.video').magnificPopup({
 type: 'iframe',
 iframe: {
@@ -23,20 +31,19 @@ callbacks: {
 });
 // Cache selectors
 var lastId,
-  topMenu = $(".ulmenu"),
-  topMenuHeight = topMenu.outerHeight()+15,
-  // All list items
-  menuItems = topMenu.find("a"),
-  // Anchors corresponding to menu items
-  scrollItems = menuItems.map(function(){
-    var item = $($(this).attr("href"));
-    if (item.length) { return item; }
-  });
+topMenu = $(".ulmenu"),
+topMenuHeight = topMenu.outerHeight()+15,
+// All list items
+menuItems = topMenu.find("a"),
+// Anchors corresponding to menu items
+scrollItems = menuItems.map(function(){
+  var item = $($(this).attr("href"));
+  if (item.length) { return item; }
+});
 // Bind click handler to menu items
-// so we can get a fancy scroll animation
 menuItems.click(function(e){
   var href = $(this).attr("href"),
-    offsetTop = href === "#" ? 0 : $(href).offset().top-topMenuHeight+1;
+  offsetTop = href === "#" ? 0 : $(href).offset().top-topMenuHeight+1;
   $('html, body').stop().animate({
     scrollTop: offsetTop
   }, 300);
@@ -48,19 +55,23 @@ $(window).scroll(function(){
   var fromTop = $(this).scrollTop()+topMenuHeight;
   // Get id of current scroll item
   var cur = scrollItems.map(function(){
-  if ($(this).offset().top < fromTop)
-  return this;
+    if ($(this).offset().top < fromTop)
+    return this;
   });
   // Get the id of the current element
   cur = cur[cur.length-1];
   var id = cur && cur.length ? cur[0].id : "";
-
   if (lastId !== id) {
-  lastId = id;
-  // Set/remove active class
-  menuItems
-    .parent().removeClass("active")
-    .end().filter("[href='#"+id+"']").parent().addClass("active");
+    lastId = id;
+    // Set/remove active class
+    menuItems
+      .parent().removeClass("active")
+      .end().filter("[href='#"+id+"']").parent().addClass("active");
   }
 });
+new WOW(
+  {
+  boxClass: 'WOW',
+  animateClass: 'animated'
+  })
 new WOW().init();
